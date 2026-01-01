ccab7e5559eb19142bc77bb9ea9fa7cd26cf6626
maxpleaner <maxpleaner@gmail.com>
2026-01-01 15:39:02 -0800

    Ensure base path always ends with trailing slash

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.BASE_PATH || '/',
+  base: (process.env.BASE_PATH || '/').replace(/\/?$/, '/'),
 })
 
